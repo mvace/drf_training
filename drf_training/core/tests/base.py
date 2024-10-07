a620d380f5145cc35d17778ae0277a939611f539
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2024-10-07 08:39:39 +0200

    Book - CRUD and list tests completed

--- drf_training/core/tests/base.py
+++ drf_training/core/tests/base.py
@@ -17,37 +17,58 @@ class BaseAPITestCase(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.published_date = datetime(2018, 9, 1)
-        self.birth_date = datetime(1980, 9, 1)
-        self.user = User.objects.create_user(
-            username="testuser", password="testpass123"
-        )
-        self.client.force_authenticate(user=self.user)
-        self.author = Author.objects.create(
-            name="John Author", birth_date=self.birth_date
-        )
-        self.book_data = {
-            "title": "Test Book",
-            "published_date": self.published_date.date().isoformat(),
-            "author": self.author.id,
-        }
-
-    def _create_book(self):
-        url = reverse(self.listURL())
-        response = self.client.post(url, self.book_data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Book.objects.count(), 1)
-        self.assertEqual(Book.objects.get().title, "Test Book")
-        created_book = Book.objects.get()
-        self.assertEqual(created_book.owner, self.user)
-
-    def _get_book(self):
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+        birth_date = datetime(1980, 9, 1)
+        self.author = Author.objects.create(name="Test Author", birth_date=birth_date)
         self.book = Book.objects.create(
             title="Test Book",
-            published_date=self.published_date,
+            published_date=datetime(2020, 1, 1),
             author=self.author,
             owner=self.user,
         )
+        self.client.force_authenticate(user=self.user)
+
+    def _list_books(self):
+        url = reverse(self.listURL())
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["title"], "Test Book")
+
+    def _create_book(self):
+        url = reverse(self.listURL())
+        data = {
+            "title": "New Book",
+            "published_date": "2021-01-01",
+            "author": self.author.id,
+            "owner": self.user.id,
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+        self.assertEqual(response.data["title"], "New Book")
+
+    def _retrieve_book(self):
         url = reverse(self.detailURL(), kwargs={"pk": self.book.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], "Test Book")
+
+    def _update_book(self):
+        url = reverse(self.detailURL(), kwargs={"pk": self.book.id})
+        data = {
+            "title": "Updated Book Title",
+            "published_date": "2022-01-01",
+            "author": self.author.id,
+            "owner": self.user.id,
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Book Title")
+
+    def _delete_book(self):
+        url = reverse(self.detailURL(), kwargs={"pk": self.book.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
--- drf_training/core/tests/test_cbv_concrete_generic_views.py
+++ drf_training/core/tests/test_cbv_concrete_generic_views.py
@@ -11,8 +11,17 @@ class BookCBVconcreteGenericViewTestCase(BaseAPITestCase):
         print("Running: book-detail-concrete-generic-cbv")
         return "book-detail-concrete-generic-cbv"
 
+    def test_list_books(self):
+        self._list_books()
+
     def test_create_book(self):
         self._create_book()
 
-    def test_get_book(self):
-        self._get_book()
+    def test_retrieve_book(self):
+        self._retrieve_book()
+
+    def test_update_book(self):
+        self._update_book()
+
+    def test_delete_book(self):
+        self._delete_book()
